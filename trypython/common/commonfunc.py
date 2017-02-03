5a2c2c05ce13511406c4044a217e110e39f34913
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-02-03 21:09:55 +0900

    チャンク関数追加

--- trypython/common/commonfunc.py
+++ trypython/common/commonfunc.py
@@ -1,5 +1,8 @@
 # coding: utf-8
 
+from typing import Iterable, Iterator, Any
+
+
 def pr(prefix: str, message: str) -> None:
     """
     指定された値を「＝」で繋いで出力します。
@@ -9,3 +12,15 @@ def pr(prefix: str, message: str) -> None:
     :return: 無し
     """
     print(f'{prefix}={message}')
+
+
+def chunks(sequence: Iterable, chunk_size: int = 1) -> Iterator[Any]:
+    """
+    指定されたシーケンスを指定されたチャンクに分割します.
+
+    :param sequence: シーケンス
+    :param chunk_size: チャンクサイズ
+    :return: Iterator[Any]
+    """
+    for i in range(0, len(sequence), chunk_size):
+        yield sequence[i:i + chunk_size]
