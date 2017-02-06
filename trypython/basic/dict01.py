655c677f3b669d5ff6526efad0985b2c810d03e4
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-02-06 15:57:23 +0900

    コード追加

--- trypython/basic/dict01.py
+++ trypython/basic/dict01.py
@@ -4,11 +4,27 @@
 ディクショナリについてのサンプルです。
 """
 from trypython.common.commoncls import SampleBase
+from trypython.common.commonfunc import pr
 
 
 class Sample(SampleBase):
     def exec(self):
-        pass
+        #
+        # ディクショナリ初期生成
+        #   空のディクショナリを作成するには、{}とするかdict()とする
+        #   空のディクショナリは、ifでFalse扱いとなる
+        #
+        dict1 = {}
+        pr('{}で作成', dict1)
+
+        if not dict1:
+            pr('{}のif判定', 'False')
+
+        dict1 = dict()
+        pr('dict()で作成', dict1)
+
+        if not dict1:
+            pr('dict()のif判定', 'False')
 
 
 def go():
