9ea9113fa994596a9dff0c7b677939da1ffb0cfd
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-02-06 11:33:55 +0900

    なんかコードが変になっていたので修正

--- trypython/basic/list01.py
+++ trypython/basic/list01.py
@@ -10,148 +10,145 @@ from trypython.common.commonfunc import *
 
 
 class Sample(SampleBase):
-    if __name__ == '__main__':
-        if __name__ == '__main__':
-            if __name__ == '__main__':
-                def exec(self):
-                    #
-                    # リスト初期生成
-                    #   空のリストを作成するには, []とするかlist()とする
-                    #   空のリストは、ifでFalse扱いとなる
-                    #
-                    # 空リスト生成
-                    list1 = []
-                    pr('[]で生成', list1)
+    def exec(self):
+        #
+        # リスト初期生成
+        #   空のリストを作成するには, []とするかlist()とする
+        #   空のリストは、ifでFalse扱いとなる
+        #
+        # 空リスト生成
+        list1 = []
+        pr('[]で生成', list1)
 
-                    if not list1:
-                        pr('[]のif判定', 'False')
+        if not list1:
+            pr('[]のif判定', 'False')
 
-                    # 空リスト生成
-                    list1 = list()
-                    pr('list()で生成', list1)
+        # 空リスト生成
+        list1 = list()
+        pr('list()で生成', list1)
 
-                    if not list1:
-                        pr('list()のif判定', 'False')
+        if not list1:
+            pr('list()のif判定', 'False')
 
-                    # 値を指定して初期化
-                    list1 = [1, 2, 3, 4, 5]
-                    pr('値を指定して初期化', list1)
+        # 値を指定して初期化
+        list1 = [1, 2, 3, 4, 5]
+        pr('値を指定して初期化', list1)
 
-                    #
-                    # 要素の追加
-                    #
-                    list1.append(10)
-                    pr('appendで要素の追加', list1)
+        #
+        # 要素の追加
+        #
+        list1.append(10)
+        pr('appendで要素の追加', list1)
 
-                    #
-                    # 要素の挿入
-                    #
-                    list1.insert(0, -1)
-                    pr('insertで要素の挿入', list1)
+        #
+        # 要素の挿入
+        #
+        list1.insert(0, -1)
+        pr('insertで要素の挿入', list1)
 
-                    # insertの場合、インデックスが要素数を超えていても例外は出ず、末尾に追加される。
-                    list1.insert(99, -100)
-                    pr('insertで範囲外のインデックス指定', list1)
+        # insertの場合、インデックスが要素数を超えていても例外は出ず、末尾に追加される。
+        list1.insert(99, -100)
+        pr('insertで範囲外のインデックス指定', list1)
 
-                    #
-                    # 要素の削除
-                    #
-                    # 特定のインデックスを削除する場合は del 文
-                    del list1[-1]
-                    pr('del文で要素の削除', list1)
+        #
+        # 要素の削除
+        #
+        # 特定のインデックスを削除する場合は del 文
+        del list1[-1]
+        pr('del文で要素の削除', list1)
 
-                    # リスト内の該当する値を削除するには remove メソッド
-                    list1.remove(10)
-                    pr('removeメソッドで値の削除', list1)
+        # リスト内の該当する値を削除するには remove メソッド
+        list1.remove(10)
+        pr('removeメソッドで値の削除', list1)
 
-                    #
-                    # クリア
-                    #
-                    list1.clear()
-                    pr('要素のクリア', list1)
+        #
+        # クリア
+        #
+        list1.clear()
+        pr('要素のクリア', list1)
 
-                    #
-                    # 他のリストからの更新
-                    #
-                    list2 = list(range(10))
-                    list1.extend(list2)
-                    pr('extendで他のリストの要素を取込む', list1)
+        #
+        # 他のリストからの更新
+        #
+        list2 = list(range(10))
+        list1.extend(list2)
+        pr('extendで他のリストの要素を取込む', list1)
 
-                    # +=を利用しても同じ
-                    list1.clear()
-                    list1 += list2
-                    pr('+=で他のリストを指定', list1)
+        # +=を利用しても同じ
+        list1.clear()
+        list1 += list2
+        pr('+=で他のリストを指定', list1)
 
-                    #
-                    # list()にデータを渡してリスト生成
-                    # 指定できるデータは iterable なものとなる
-                    #
-                    original_data1 = 'helloworld'
-                    list3 = list(original_data1)
-                    pr('list()にデータを指定してリスト生成', list3)
+        #
+        # list()にデータを渡してリスト生成
+        # 指定できるデータは iterable なものとなる
+        #
+        original_data1 = 'helloworld'
+        list3 = list(original_data1)
+        pr('list()にデータを指定してリスト生成', list3)
 
-                    # not iterableなデータを渡すと例外 (TypeError)
-                    try:
-                        list(100)
-                    except TypeError as e:
-                        pr('list()に not iterableを指定', e)
+        # not iterableなデータを渡すと例外 (TypeError)
+        try:
+            list(100)
+        except TypeError as e:
+            pr('list()に not iterableを指定', e)
 
-                    #
-                    # pop() を利用して要素の取り出し
-                    # pythonのListはキューやスタックとしても利用可能
-                    #   (1) appendしてpop(-1)すると、スタック
-                    #   (2) appendしてpop(0)すると、キュー
-                    #
-                    list4 = list(range(10))
-                    pr('pop()呼び出し前', list4)
-                    pr('指定なしでpop()呼び出し', list4.pop())  # 引数なしはインデックスに -1 を指定した事になる
-                    pr('pop()呼び出し後', list4)
-                    pr('インデックス指定でpop()呼び出し', list4.pop(2))
-                    pr('pop()呼び出し後', list4)
+        #
+        # pop() を利用して要素の取り出し
+        # pythonのListはキューやスタックとしても利用可能
+        #   (1) appendしてpop(-1)すると、スタック
+        #   (2) appendしてpop(0)すると、キュー
+        #
+        list4 = list(range(10))
+        pr('pop()呼び出し前', list4)
+        pr('指定なしでpop()呼び出し', list4.pop())  # 引数なしはインデックスに -1 を指定した事になる
+        pr('pop()呼び出し後', list4)
+        pr('インデックス指定でpop()呼び出し', list4.pop(2))
+        pr('pop()呼び出し後', list4)
 
-                    # スタック的な使い方 (LIFO)
-                    a_stack = list(range(2))
-                    a_stack.append(5)
-                    pr('要素の追加 (append)(Stack)', a_stack)
-                    pr('要素の取得 (pop)(Stack)', a_stack.pop())
+        # スタック的な使い方 (LIFO)
+        a_stack = list(range(2))
+        a_stack.append(5)
+        pr('要素の追加 (append)(Stack)', a_stack)
+        pr('要素の取得 (pop)(Stack)', a_stack.pop())
 
-                    # キュー的な使い方 (FIFO)
-                    a_queue = list(range(2))
-                    a_queue.append(5)
-                    pr('要素の追加 (append)(Queue)', a_queue)
-                    pr('要素の取得 (pop)(Queue)', a_queue.pop(0))
+        # キュー的な使い方 (FIFO)
+        a_queue = list(range(2))
+        a_queue.append(5)
+        pr('要素の追加 (append)(Queue)', a_queue)
+        pr('要素の取得 (pop)(Queue)', a_queue.pop(0))
 
-                    #
-                    # リストのスライス
-                    # [start:end:step]で指定できる
-                    # 負のインデックスは末尾からのインデックスとなる
-                    #   ex) -1は最終要素, -2は最終要素の一つ前
-                    # 通常のインデックス指定の場合は、要素数よりも大きい数を
-                    # 指定すると例外が発生するが、スライスの場合は発生しない。
-                    # stepを指定すると、指定した数分飛ばして要素取得する。
-                    # stepに負の値を指定すると、最終要素からとなる。
-                    # stepに -1 を指定すると、リストを reverse することになる
-                    #
-                    list5 = list(range(10))
-                    pr('最初から5番目まで取得', list5[:5])  # startを指定しない場合は 0 を指定した事になる
-                    pr('最初から5番目まで２つずつ取得', list5[:5:2])
-                    pr('3番目から末尾まで取得', list5[2:])  # endを指定しない場合は 末尾まで を指定した事になる
-                    pr('逆順で取得(reverse)', list5[::-1])
-                    pr('逆順で２つずつ取得', list5[::-2])
-                    pr('末尾の2番めから正順の2番目まで３つずつ取得', list5[-2:1:-3])
+        #
+        # リストのスライス
+        # [start:end:step]で指定できる
+        # 負のインデックスは末尾からのインデックスとなる
+        #   ex) -1は最終要素, -2は最終要素の一つ前
+        # 通常のインデックス指定の場合は、要素数よりも大きい数を
+        # 指定すると例外が発生するが、スライスの場合は発生しない。
+        # stepを指定すると、指定した数分飛ばして要素取得する。
+        # stepに負の値を指定すると、最終要素からとなる。
+        # stepに -1 を指定すると、リストを reverse することになる
+        #
+        list5 = list(range(10))
+        pr('最初から5番目まで取得', list5[:5])  # startを指定しない場合は 0 を指定した事になる
+        pr('最初から5番目まで２つずつ取得', list5[:5:2])
+        pr('3番目から末尾まで取得', list5[2:])  # endを指定しない場合は 末尾まで を指定した事になる
+        pr('逆順で取得(reverse)', list5[::-1])
+        pr('逆順で２つずつ取得', list5[::-2])
+        pr('末尾の2番めから正順の2番目まで３つずつ取得', list5[-2:1:-3])
 
-                    #
-                    # 要素のインデックスを取得
-                    #
-                    list6 = [1, 100, 99, 3, 6]
-                    pr('値が 99 のインデックス', list6.index(99))
+        #
+        # 要素のインデックスを取得
+        #
+        list6 = [1, 100, 99, 3, 6]
+        pr('値が 99 のインデックス', list6.index(99))
 
-                    #
-                    # 指定した値が存在するか
-                    #
-                    list7 = list(range(20))
-                    pr('値が 11 はリスト内に存在するか', 11 in list7)
-                    pr('値が 99 はリスト内に存在するか', 99 in list7)
+        #
+        # 指定した値が存在するか
+        #
+        list7 = list(range(20))
+        pr('値が 11 はリスト内に存在するか', 11 in list7)
+        pr('値が 99 はリスト内に存在するか', 99 in list7)
 
 
 def go():
