ee45045d6732463a5337146cd2c9cfb5a83a5787
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-02-10 00:28:04 +0900

    コード追加

---
+++ trypython/stdlib/itertools01.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+
+"""
+itertools についてのサンプルです。
+"""
+import itertools
+
+from trypython.common.commoncls import SampleBase
+from trypython.common.commonfunc import pr
+
+
+class Sample(SampleBase):
+    def exec(self):
+        #
+        # itertools.chain()
+        # 複数のシーケンスを繋いで一つのようにする
+        #
+        list01 = ['hello', 'world']
+        list02 = list(range(10))
+
+        for x in itertools.chain(list01, list02, 'abc'):
+            pr('value', x)
+
+        #
+        # itertools.zip_longest()
+        # 組み込み関数 zip() の別バージョン
+        # 要素数が多い方に合わせる
+        #
+        # zip() は、要素が最も少ない方になる
+        for x, y in zip(list01, list02):
+            pr('zip()', (x, y))
+
+        # zip_longest()は、要素が最も多い方になる
+        for x, y in itertools.zip_longest(list01, list02):
+            pr('zip_longest()', (x, y))
+
+
+def go():
+    obj = Sample()
+    obj.exec()
+
+
+if __name__ == '__main__':
+    go()
