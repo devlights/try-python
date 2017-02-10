8f6c3b25de1a3f303fe7b39bf786e61d95b285ee
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-02-10 12:33:56 +0900

    コード追加

---
+++ trypython/basic/func01.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+
+"""
+関数についてのサンプルです。
+"""
+from trypython.common.commoncls import SampleBase
+from trypython.common.commonfunc import pr
+
+
+class Sample(SampleBase):
+    def exec(self):
+        #
+        # オプション引数
+        #
+        self.func_with_default_val()
+
+        #
+        # *による位置引数のタプル化
+        #
+        self.func_with_args_tuples(1, 2, 'hello', 'world')
+
+        #
+        # **によるキーワード引数の辞書化
+        #
+        self.func_with_kwargs_dict(name='hello', name2='world')
+
+        #
+        # 全部入り
+        #
+        self.func_allin('helloworld', 1, 2, 'こんにちわ世界', value1=100, value2='string value')
+
+    def func_with_default_val(self, message=''):
+        pr('func_with_default_val', message)
+
+    def func_with_args_tuples(self, *args):
+        pr('func_with_args_tuples', args)
+
+    def func_with_kwargs_dict(self, **kwargs):
+        pr('func_with_kwargs_dict', kwargs)
+
+    def func_allin(self, message='', *args, **kwargs):
+        pr('func_allin', (message, args, kwargs,))
+
+
+def go():
+    obj = Sample()
+    obj.exec()
+
+
+if __name__ == '__main__':
+    go()
