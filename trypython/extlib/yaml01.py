df4263c24bb6fd3fe72ceaee93d13dd18d1b8d37
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-02-13 12:41:09 +0900

    add yaml01.py

---
+++ trypython/extlib/yaml01.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+"""
+pyyamlモジュールについてのサンプルです。
+"""
+import yaml
+
+from trypython.common.commoncls import SampleBase
+from trypython.common.commonfunc import pr
+
+
+class Sample(SampleBase):
+    def exec(self):
+        #
+        # python で yaml を扱う場合、標準モジュールには
+        # 存在しないので、PyYAMLモジュールを利用する
+        #
+        with open('sample01.yaml') as fp:
+            obj = yaml.load(fp)
+            pr('yaml.load()', obj)
+
+
+def go():
+    obj = Sample()
+    obj.exec()
+
+
+if __name__ == '__main__':
+    go()
