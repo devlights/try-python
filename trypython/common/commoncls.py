6928508bba3119ddb46d936b58fe0bc649c61873
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-02-14 18:14:41 +0900

    add timetracer class

--- trypython/common/commoncls.py
+++ trypython/common/commoncls.py
@@ -3,13 +3,29 @@
 """
 共通クラスが定義されています。
 """
-from abc import *
+import contextlib
+from datetime import datetime
+from abc import ABCMeta, abstractmethod
 
 
 class SampleBase(metaclass=ABCMeta):
     """
     サンプルクラスの基底となるクラスです。
     """
+
     @abstractmethod
     def exec(self):
         raise NotImplementedError()
+
+
+class timetracer(contextlib.AbstractContextManager):
+    def __init__(self, message: str):
+        self._message = message
+        self._start = None
+
+    def __enter__(self):
+        self._start = datetime.now()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        diff = (datetime.now() - self._start)
+        print(f'[{self._message}] elapsed: {diff.seconds}.{diff.microseconds} seconds')
