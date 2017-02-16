3a573c4117281c9fc4fdbae94fc209c83db8d192
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-02-16 16:56:51 +0900

    update some code

--- trypython/advanced/coroutine01.py
+++ trypython/advanced/coroutine01.py
@@ -7,7 +7,7 @@
   http://postd.cc/python-generators-coroutines-native-coroutines-and-async-await/
   https://docs.python.jp/3/library/asyncio-task.html
 """
-from typing import Generator
+from typing import Generator, Coroutine
 
 from trypython.common.commoncls import SampleBase
 from trypython.common.commonfunc import pr
@@ -15,19 +15,77 @@ from trypython.common.commonfunc import pr
 
 class Sample(SampleBase):
     def exec(self):
+        #
+        # co-routineの基本は、generatorから。
+        # generatorは、値を生成する関数。
+        # generatorからは、値をpullすることができる。
+        # 逆に値を push したい場合は？
+        # このような場合に co-routine を利用する。
+        #
+        # generatorに対して値を push する、つまりコルーチンの処理を
+        # するには、send() を利用する。
+        #
         gen01 = simple_generator()
         try:
+            pr('gen01-type', type(gen01))
             pr('gen01', next(gen01))
             pr('gen01', next(gen01))
             pr('gen01', next(gen01))
         except StopIteration as e:
             pr('gen01', e)
 
+        cor01 = simple_coroutine()
+        try:
+            pr('cor01-type', type(cor01))
+            pr('cor01', next(cor01))
+            pr('cor01', next(cor01))
+            pr('cor01', next(cor01))
+        except StopIteration as e:
+            pr('cor01', e)
 
-def simple_generator() -> Generator[str, None, None]:
+        cor02 = simple_coroutine()
+        try:
+            pr('cor02-type', type(cor02))
+            pr('cor02-pull', next(cor02))
+            pr('cor02-push', cor02.send('world'))
+            pr('cor02', next(cor02))
+        except StopIteration as e:
+            pr('cor02', e)
+
+        cor03 = simple_coroutine2()
+        try:
+            pr('cor03.pull', next(cor03))
+            pr('cor03.push', cor03.send(10), 'send(10)')
+            pr('cor03.push', cor03.send(20), 'send(20)')
+            pr('cor03.push', cor03.send(30), 'send(30)')
+            pr('cor03.push', cor03.send(40), 'send(40)')
+            pr('cor03.push', cor03.send(50), 'send(50)')
+            pr('cor03.push', cor03.send(-1), 'send(-1)')
+        except StopIteration as e:
+            pr('cor03', e)
+
+
+def simple_generator():
     yield 'hello'
     yield 'world'
 
+
+def simple_coroutine():
+    hello = (yield 'hello')
+    yield hello
+
+
+def simple_coroutine2():
+    total = 0
+    while True:
+        push_val = yield total
+        pr('\tpush_val', push_val)
+        if (push_val < 0):
+            pr('break-cor03', push_val)
+            break
+        total += push_val
+    pr('cor03', 'before last yield total')
+    yield total
 
 def go():
     obj = Sample()
