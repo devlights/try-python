1b658ba0aaf24ba7d3aed6a55323ee41b5390cf1
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-02-16 15:01:11 +0900

    add some code

--- trypython/advanced/coroutine01.py
+++ trypython/advanced/coroutine01.py
@@ -2,13 +2,32 @@
 
 """
 コルーチン (co-routine) についてのサンプルです。
+
+参考:
+  http://postd.cc/python-generators-coroutines-native-coroutines-and-async-await/
+  https://docs.python.jp/3/library/asyncio-task.html
 """
+from typing import Generator
+
 from trypython.common.commoncls import SampleBase
+from trypython.common.commonfunc import pr
 
 
 class Sample(SampleBase):
     def exec(self):
-        pass
+        gen01 = simple_generator()
+
+        pr('gen01', next(gen01))
+        pr('gen01', next(gen01))
+        try:
+            pr('gen01', next(gen01))
+        except StopIteration as e:
+            pr('gen01', e)
+
+
+def simple_generator() -> Generator[str, None, None]:
+    yield 'hello'
+    yield 'world'
 
 
 def go():
