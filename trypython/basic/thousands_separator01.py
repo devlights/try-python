98c0e20e0357dfbbea54e74c5eeb4d37b2eb7f37
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-03-06 13:24:55 +0900

    add thousands_separator01.py

---
+++ trypython/basic/thousands_separator01.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+"""
+1000区切りのカンマで書式出力するための方法についてのサンプルです。
+"""
+from trypython.common.commoncls import SampleBase
+from trypython.common.commonfunc import pr
+
+
+class Sample(SampleBase):
+    def exec(self):
+        i = 123456789
+        pr('thousands separator', str.format('{0:,d}', i))
+        pr('thousands separator', str.format('{0:20,d}', i))
+
+
+def go():
+    obj = Sample()
+    obj.exec()
+
+
+if __name__ == '__main__':
+    go()
