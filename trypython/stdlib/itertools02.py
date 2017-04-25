388e25b035fae4be206bf7c4838e8a6d597d36ad
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-04-25 15:43:39 +0900

    add itertools02.py

---
+++ trypython/stdlib/itertools02.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+"""itertools.cycleについてのサンプルです。"""
+import itertools
+
+from trypython.common.commoncls import SampleBase
+from trypython.common.commonfunc import pr
+
+
+class Sample(SampleBase):
+    def exec(self):
+        # -------------------------------------------
+        # itertools.cycleは、名前の通り
+        # 最初に指定したシーケンスを繰り返しサイクルする
+        # イテレータを生成してくれる。
+        #
+        # これで生成したイテレータは無限にサイクルするので
+        # ストップさせるのは、利用者側の責任になる。
+        # -------------------------------------------
+        numbers = list(range(0, 9))
+        cycle_iter = itertools.cycle((1, 2, 3))
+
+        for i, j in zip(numbers, cycle_iter):
+            pr("i,j", f'{i}-{j}')
+
+
+def go():
+    obj = Sample()
+    obj.exec()
+
+
+if __name__ == '__main__':
+    go()
