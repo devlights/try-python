3eb2638cbcd4ad874669e7fd5666d107c1c407c2
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-04-28 22:09:19 +0900

    add itertools03.py

---
+++ trypython/stdlib/itertools03.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+"""itertools.repeat()についてのサンプルです。"""
+import itertools
+
+from trypython.common.commoncls import SampleBase
+from trypython.common.commonfunc import pr
+
+
+class Sample(SampleBase):
+    def exec(self):
+        # -----------------------------------------------
+        # itertools.repeat() は、指定したオブジェクトを
+        # 指定した回数分繰り返すイテレータを生成してくれる。
+        # 繰り返し回数は、第二引数の times で指定できる。
+        # timesのデフォルトは None となっており、これは
+        # 無限の繰り返しを表す。
+        # -----------------------------------------------
+        str01 = 'hello python'
+        for x in itertools.repeat(str01, 2):
+            pr('itertools-repeat', x)
+
+        list01 = list(range(5))
+        for i, x in enumerate(itertools.repeat(list01)):
+            # そのままだと、無限ループするので10回出力で止める
+            if i >= 10:
+                break
+            pr('itertools-repeat times=None', i, x)
+
+
+def go():
+    obj = Sample()
+    obj.exec()
+
+
+if __name__ == '__main__':
+    go()
