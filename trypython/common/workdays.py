9627626f8badd47b7277a6ed9ee5499f8bdbe486
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-05-08 15:56:58 +0900

    update doctest

--- trypython/common/workdays.py
+++ trypython/common/workdays.py
@@ -25,6 +25,25 @@ def get_day(base_day, days_count=1, optional_holidays=(), optional_business_days
 
     >>> get_day(today, 6, opt_holidays, opt_business_days)
     datetime.date(2017, 2, 14)
+    
+    >>> from datetime import datetime
+    >>> d1 = datetime(2017, 2, 6)
+    >>> get_day(d1)
+    Traceback (most recent call last):
+    AssertionError: base_day is not type(date)
+    
+    >>> d1 = date(2017, 2, 6)
+    >>> opt_busi_days = (datetime(2017, 2, 13), datetime(2017, 2, 14))
+    >>> get_day(d1, optional_business_days=opt_busi_days)
+    Traceback (most recent call last):
+    AssertionError: optional_business_days is not type(date)
+
+    >>> d1 = date(2017, 2, 13)
+    >>> opt_busi_days = (date(2017, 2, 13), date(2017, 2, 14))
+    >>> opt_holi_days = (datetime(2017, 2, 11), datetime(2017, 2, 12))
+    >>> get_day(d1, optional_business_days=opt_busi_days, optional_holidays=opt_holi_days)
+    Traceback (most recent call last):
+    AssertionError: optional_holidays is not type(date)
 
     :param date base_day: 基準日
     :param int days_count: 日数
@@ -56,41 +75,5 @@ def get_day(base_day, days_count=1, optional_holidays=(), optional_business_days
 
 
 if __name__ == '__main__':
-    from datetime import datetime
-
-    try:
-        d1 = datetime(2017, 2, 13)
-        get_day(d1)
-    except AssertionError as e:
-        print('ok', e)
-    else:
-        print('ng')
-
-    try:
-        d1 = date(2017, 2, 13)
-        opt_busi_days = (datetime(2017, 2, 13), datetime(2017, 2, 14))
-        get_day(d1, optional_business_days=opt_busi_days)
-    except AssertionError as e:
-        print('ok', e)
-    else:
-        print('ng')
-
-    try:
-        d1 = date(2017, 2, 13)
-        opt_busi_days = (date(2017, 2, 13), date(2017, 2, 14))
-        opt_holi_days = (datetime(2017, 2, 11), datetime(2017, 2, 12))
-        get_day(d1, optional_business_days=opt_busi_days, optional_holidays=opt_holi_days)
-    except AssertionError as e:
-        print('ok', e)
-    else:
-        print('ng')
-
-    try:
-        d1 = date(2017, 2, 13)
-        opt_busi_days = (date(2017, 2, 13), date(2017, 2, 14))
-        opt_holi_days = (date(2017, 2, 11), date(2017, 2, 12))
-        ret = get_day(d1, optional_business_days=opt_busi_days, optional_holidays=opt_holi_days)
-    except AssertionError as e:
-        print('ng', e)
-    else:
-        print('ok', ret)
+    import doctest
+    doctest.testmod(verbose=True)
