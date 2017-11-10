7e5a507340f1e57f74bce9844f28fec28d198dd8
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-11-10 20:37:34 +0900

    Update comments

--- trypython/advanced/iter01.py
+++ trypython/advanced/iter01.py
@@ -20,6 +20,27 @@ class Sample(SampleBase):
         self._file = '/tmp/test.txt'
 
     def exec(self):
+        ####################################################
+        # iter(callable, sentinel)
+        #
+        # 組み込み関数の iter は、引数が一つの場合と
+        # 二つの場合で挙動が異なる。
+        #
+        # 引数が一つの場合、要求される引数は iterable だが
+        # 引数が二つの場合、要求される引数は
+        #    callable と sentinel
+        # となる。
+        #
+        # sentinel は、日本語で言うと「番兵」の事。
+        # iter(callable, sentinel) は、毎回 calable を
+        # 呼び出して、その値が sentinel と等しいかを判定する。
+        #
+        # 等しい場合は、StopIteration が発生して
+        # イテレーションが終わる。
+        #
+        # なので、何かの値が出てくるまで継続するという
+        # 処理と記述する時に使える。
+        ####################################################
         try:
             self._write_dummy_file()
 
