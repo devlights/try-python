c384005f85161cbabb4784198f3df1d5b2d05b98
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-12-07 18:09:04 +0900

    Update script

--- trypython/stdlib/weakref02.py
+++ trypython/stdlib/weakref02.py
@@ -31,9 +31,39 @@ class Sample(SampleBase):
         pr('__weakref__ exists?', hasattr(a, '__weakref__'))
         pr('a.__weakref__ is wref', a.__weakref__ is wref)
 
+        # ------------------------------------------------
+        # ユーザ定義クラスで弱参照をサポートするためには
+        # __weakref__ 属性が必要となる。__slots__ 属性を
+        # つかって、オブジェクトのメモリサイズを最適化している
+        # 場合、この属性を含めていないと弱参照が作れない。
+        # ------------------------------------------------
+        b = NotContainsWeakrefAttr(100)
+        try:
+            bref = weakref.ref(b)
+        except TypeError as e:
+            pr('NotContainsWeakrefAttr', e)
+
+        c = ContainsWeakrefAttr(100)
+        cref = weakref.ref(c)
+        print(cref)
+
 
 class A:
     pass
+
+
+class NotContainsWeakrefAttr:
+    __slots__ = ('val',)
+
+    def __init__(self, val):
+        self.val = val
+
+
+class ContainsWeakrefAttr:
+    __slots__ = ('val', '__weakref__',)
+
+    def __init__(self, val):
+        self.val = val
 
 
 def go():
