0325261585dca797bf77a028790abc5e2134e2bc
devlights <gsf.zero1@gmail.com>
2018-03-01 14:55:17 +0900

    add str in multiple items sample

---
+++ trypython/basic/str_in_multiple01.py
@@ -0,0 +1,30 @@
+"""
+文字列に対する in 操作で複数の項目に対して一発で含むものが存在するかのサンプル
+"""
+from trypython.common.commoncls import SampleBase
+from trypython.common.commonfunc import pr
+
+
+class Sample(SampleBase):
+    def exec(self):
+        # 基本的なやり方
+        target_str = 'abcede hello world 12345'
+
+        items = 'z y x hello'.split()
+        for x in items:
+            if x in target_str:
+                pr('basic', f'found: {x}')
+
+        # any を使ったやり方
+        found = any(x in target_str for x in items)
+        if found:
+            pr('any', f'found')
+
+
+def go():
+    obj = Sample()
+    obj.exec()
+
+
+if __name__ == '__main__':
+    go()
