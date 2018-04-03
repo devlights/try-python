cd9608b0963635e48a88e5b17578f1188881361c
devlights <gsf.zero1@devlights.onmicrosoft.com>
2018-04-03 14:21:21 +0900

    update yaml sample

--- trypython/extlib/yaml01.py
+++ trypython/extlib/yaml01.py
@@ -3,10 +3,27 @@
 """
 pyyamlモジュールについてのサンプルです。
 """
+import io
 import yaml
 
 from trypython.common.commoncls import SampleBase
 from trypython.common.commonfunc import pr
+
+YAML_TEXT = """\
+# コメント
+
+# 辞書の中にリスト
+mydata1:
+  - hello
+  - world
+
+mydata2:
+  # リストの中に辞書
+  - key1: hello
+    key2: world
+  - key3: hehe
+    key4: hoge
+"""
 
 
 class Sample(SampleBase):
@@ -15,7 +32,7 @@ class Sample(SampleBase):
         # python で yaml を扱う場合、標準モジュールには
         # 存在しないので、PyYAMLモジュールを利用する
         #
-        with open('sample01.yaml', encoding='utf-8') as fp:
+        with io.StringIO(YAML_TEXT) as fp:
             obj = yaml.load(fp)
             pr('yaml.load()', obj)
 
