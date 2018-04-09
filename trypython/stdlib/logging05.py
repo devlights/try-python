d8eaf7ea9c2f65d8e1b7023f84a14a7b39438694
devlights <gsf.zero1@devlights.onmicrosoft.com>
2018-04-09 14:04:05 +0900

    add links

--- trypython/stdlib/logging05.py
+++ trypython/stdlib/logging05.py
@@ -1,4 +1,9 @@
 """
 logging モジュールのサンプルです。
 応用編 (ハンドラの指定)
+
+参考情報::
+https://stackoverflow.com/questions/6167587/the-logging-handlers-how-to-rollover-after-time-or-maxbytes
+https://stackoverflow.com/questions/29602352/how-to-mix-logging-handlers-file-timed-and-compress-log-in-the-same-config-f
+https://docs.python.jp/3/howto/logging-cookbook.html#using-file-rotation
 """
