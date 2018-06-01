84c575422cff4d82cbc8b68d84cde5e5a46abb5b
devlights <gsf.zero1@devlights.onmicrosoft.com>
2018-06-01 12:14:33 +0900

    add crayons01.py

---
+++ trypython/extlib/crayons01.py
@@ -0,0 +1,33 @@
+"""
+コンソール上に色付き文字列を表示してくれるライブラリ crayons のサンプルです。
+
+REFERENCES::
+https://github.com/kennethreitz/crayons
+"""
+import crayons
+
+from trypython.common.commoncls import SampleBase
+
+
+class Sample(SampleBase):
+    """ライブラリ crayons のサンプルです。"""
+
+    def exec(self):
+        """サンプル処理を実行します。"""
+        # 赤色で太字な文字列を構築
+        red_bold_string = crayons.red('[RED] helloworld', bold=True)
+        # 黄色な文字列を構築
+        yellow_normal_string = crayons.yellow('[YELLOW] hellowworld', bold=False)
+
+        print(red_bold_string)
+        print(yellow_normal_string)
+
+
+def go():
+    """サンプルを実行します。"""
+    obj = Sample()
+    obj.exec()
+
+
+if __name__ == '__main__':
+    go()
