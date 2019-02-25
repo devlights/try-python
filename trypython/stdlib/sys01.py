9a16a624399a798eb90959e438b10fe7ca1c35a3
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-02-25 15:35:27 +0900

    add sys.executable sample

---
+++ trypython/stdlib/sys01.py
@@ -0,0 +1,34 @@
+"""
+sys モジュールに関するサンプルです。
+
+- sys.executable について
+"""
+import sys
+
+from common.commoncls import SampleBase
+from common.commonfunc import pr
+
+
+class Sample(SampleBase):
+    def exec(self):
+        # -------------------------------------------------------
+        # sys モジュール
+        # -------------
+        # sys.executable から、現在動作中の python インタープリタの
+        # 絶対パスが取得できる。 システムに複数の python がインストールされていたり
+        # venv 環境下で作業している場合に、実行パスを間違えないように
+        # ここから取得するほうが良い。
+        # -------------
+        # subprocess モジュールを使って、内部から別プロセスで python を起動する際にも
+        # sys.executable からパスを取得して起動すれば間違いがない。
+        # -------------------------------------------------------
+        pr('sys.executable', sys.executable)
+
+
+def go():
+    obj = Sample()
+    obj.exec()
+
+
+if __name__ == '__main__':
+    go()
