6f7a4440ec8644532f32458bd6d3d547acfdbce7
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-03-19 16:10:10 +0900

    add netaddr example

---
+++ trypython/extlib/netaddr/netaddr01.py
@@ -0,0 +1,42 @@
+"""
+netaddr モジュールについてのサンプルです
+
+基本的な使い方について
+
+REFERENCES:: http://bit.ly/2Oepd8H
+             http://bit.ly/2Oc12Yo
+             http://bit.ly/2OehqYv
+             http://bit.ly/2Oc5W7M
+             http://bit.ly/2ObWb9C
+
+"""
+import netaddr
+
+from trypython.common.commoncls import SampleBase
+from trypython.common.commonfunc import pr
+
+
+class Sample(SampleBase):
+    def exec(self):
+        # --------------------------------------------------------------------------
+        # netaddr モジュール
+        # ------------------------
+        # ネットワークアドレスを表現したり操作したり出来るライブラリ.
+        # 例えばIPアドレスの取扱などがとても楽に出来る
+        # --------------------------------------------------------------------------
+        ip = netaddr.IPAddress('192.168.0.111')
+        pr('netaddr.IPAddress', ip)
+        pr('ip.version', ip.version)
+        pr('ip.ipv4()', ip.ipv4())
+        pr('ip.ipv6()', ip.ipv6())
+        pr('ip.netmask_bits()', ip.netmask_bits())
+        pr('ip.words', ip.words)
+
+
+def go():
+    obj = Sample()
+    obj.exec()
+
+
+if __name__ == '__main__':
+    go()
