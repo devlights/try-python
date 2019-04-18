bbb38fcf951e6ffb8834cf4b1a0a6a4907d047a3
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-04-18 18:46:27 +0900

    tempfile.gettempdir() のサンプルを追加

---
+++ trypython/stdlib/tempfile_/tempfile02.py
@@ -0,0 +1,30 @@
+"""
+tempfile モジュールに関するサンプルです。
+
+tempfile.gettempdir() の使い方について
+
+REFERENCES:: http://bit.ly/2GqvFac
+"""
+import tempfile
+
+from trypython.common.commoncls import SampleBase
+from trypython.common.commonfunc import pr
+
+
+class Sample(SampleBase):
+    def exec(self):
+        # ----------------------------------------------
+        # tempfile.gettempdir() は
+        # そのOSで安全に利用できる一時ディレクトリのパスを
+        # 返してくれる。
+        # ----------------------------------------------
+        pr('gettempdir', tempfile.gettempdir())
+
+
+def go():
+    obj = Sample()
+    obj.exec()
+
+
+if __name__ == '__main__':
+    go()
