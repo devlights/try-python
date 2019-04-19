c736fe4f8810b093c4d08bb2f10ef7bb71b090ae
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-04-19 20:28:15 +0900

    fix CodeFactor issue

--- trypython/advanced/fileio/rmdir01.py
+++ trypython/advanced/fileio/rmdir01.py
@@ -3,7 +3,9 @@
 Python で ディレクトリの削除を行うサンプルです。
 """
 import os
+import pathlib
 import shutil
+import tempfile
 
 from trypython.common.commoncls import SampleBase
 from trypython.common.commonfunc import pr
@@ -12,34 +14,38 @@ from trypython.common.commonfunc import pr
 # noinspection PyTypeChecker
 class Sample(SampleBase):
     def exec(self):
-        os.mkdir('/tmp/test01')
-        try:
-            # os.remove ではディレクトリは削除できない
-            # 中身が空でもダメ
-            os.remove('/tmp/test01')
-        except OSError as e:
-            pr('os.remove', e)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            base_dir = pathlib.Path(tmpdir)
+            pr('base_dir', base_dir)
 
-        # 中身が空の場合は、rmdir() で削除できる
-        os.rmdir('/tmp/test01')
+            os.mkdir(base_dir / 'test01')
+            try:
+                # os.remove ではディレクトリは削除できない
+                # 中身が空でもダメ
+                os.remove(base_dir / 'test01')
+            except OSError as e:
+                pr('os.remove', e)
 
-        # サブディレクトリも含めて一気に作成
-        os.makedirs('/tmp/a/b/c/d')
+            # 中身が空の場合は、rmdir() で削除できる
+            os.rmdir(base_dir / 'test01')
 
-        # サブディレクトリも含めて一気に削除
-        # ただし、中身が空の場合のみ
-        os.removedirs('/tmp/a/b/c/d')
+            # サブディレクトリも含めて一気に作成
+            os.makedirs(base_dir / 'a/b/c/d')
 
-        # ディレクトリ内にコンテンツがあると removedirs() は失敗する
-        os.makedirs('/tmp/a/b/c')
-        print('hello world', file=open('/tmp/a/b/c/test.txt', mode='wt'))
-        try:
-            os.removedirs('/tmp/a/b/c')
-        except OSError as e:
-            pr('os.removedirs', e)
+            # サブディレクトリも含めて一気に削除
+            # ただし、中身が空の場合のみ
+            os.removedirs(base_dir / 'a/b/c/d')
 
-        # shutil.rmtree() はコンテンツがあっても再帰削除してくれる
-        shutil.rmtree('/tmp/a')
+            # ディレクトリ内にコンテンツがあると removedirs() は失敗する
+            os.makedirs(base_dir / 'a/b/c')
+            print('hello world', file=open(base_dir / 'a/b/c/test.txt', mode='wt'))
+            try:
+                os.removedirs(base_dir / 'a/b/c')
+            except OSError as e:
+                pr('os.removedirs', e)
+
+            # shutil.rmtree() はコンテンツがあっても再帰削除してくれる
+            shutil.rmtree(base_dir / 'a')
 
 
 def go():
