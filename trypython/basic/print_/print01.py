68215c460c18d8197bd6ae53649740e547e34793
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-04-19 20:49:47 +0900

    fix CodeFactor issue

--- trypython/basic/print_/print01.py
+++ trypython/basic/print_/print01.py
@@ -4,13 +4,13 @@
 """
 print関数のサンプルです.
 """
+import tempfile
+
 from trypython.common.commoncls import *
 
 
 class Sample(SampleBase):
     def exec(self) -> None:
-        import tempfile
-
         ##################################
         # print関数
         # python3からprintが関数になったため
@@ -31,7 +31,18 @@ class Sample(SampleBase):
         ##################################
         # 出力先はデフォルトで標準出力
         # 変更可能
-        tmp_file_path = tempfile.mktemp()
+        # -------------------------------------------------------------------
+        # mktemp 関数を利用していると CodeFactor で以下の警告を指摘された。
+        #
+        # Use of insecure and deprecated function (mktemp).
+        # Use of this function may introduce a security hole in your program.
+        # By the time you get around to doing anything with the file name it returns,
+        # someone else may have beaten you to the punch. Use mkstemp()
+        # instead or replace with NamedTemporaryFile(), passing it the delete=False parameter.
+        #
+        # mktemp 関数はセキュアな関数ではないので代わりに mkstemp 関数を利用せよとのこと。
+        # -------------------------------------------------------------------
+        _, tmp_file_path = tempfile.mkstemp()
         with open(tmp_file_path, "wt") as writer:
             print(message, message2, file=writer)
         with open(tmp_file_path, "r") as reader:
