7c6acd7e18846bd41ad89f486cfe2fb6645693c1
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-04-28 14:23:56 +0900

    Add list02.py (リストをflattenするサンプル)

---
+++ trypython/basic/list_/list02.py
@@ -0,0 +1,33 @@
+"""
+リストのサンプルです.
+
+リストの flatten について
+"""
+import itertools
+
+from trypython.common.commoncls import SampleBase
+from trypython.common.commonfunc import pr
+
+
+class Sample(SampleBase):
+    def exec(self):
+        # リストの要素が tuple になってるデータを生成
+        nested = [(chr(x), chr(x + 1)) for x in range(65, 70)]
+        pr('nested', nested)
+
+        # itertools.chain を使って flatten
+        flattened = list(itertools.chain.from_iterable(nested))
+        pr('flatten', flattened)
+
+        # ついでに、並び順をキープしたままで重複削除 (python 3.7 以降で有効)
+        dup_deleted = list(dict.fromkeys(flattened))
+        pr('dup_deleted', dup_deleted)
+
+
+def go():
+    obj = Sample()
+    obj.exec()
+
+
+if __name__ == '__main__':
+    go()
