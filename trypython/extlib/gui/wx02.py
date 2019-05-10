bb441068fab5e2e380b4661a405e3bdf0ba8708a
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-05-10 13:45:09 +0900

    Add wxPython example

---
+++ trypython/extlib/gui/wx02.py
@@ -0,0 +1,39 @@
+"""
+wxPython についてのサンプルです
+
+wx.App の サブクラス化 について
+"""
+import wx
+
+from trypython.common.commoncls import SampleBase
+
+
+# noinspection PyPep8Naming
+class MyApp(wx.App):
+
+    def __init__(self, redirect=False, filename=None, use_best_visual=False, clear_sig_int=True):
+        super().__init__(redirect, filename, use_best_visual, clear_sig_int)
+        self._frame = None
+
+    def OnInit(self) -> bool:
+        super().OnInit()
+        self._frame = wx.Frame(None)
+        self._frame.SetTitle('wx02.py')
+        self.SetTopWindow(self._frame)
+        self._frame.Show(True)
+        return True
+
+
+class Sample(SampleBase):
+    def exec(self):
+        app = MyApp()
+        app.MainLoop()
+
+
+def go():
+    obj = Sample()
+    obj.exec()
+
+
+if __name__ == '__main__':
+    go()
