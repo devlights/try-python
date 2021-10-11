0ba38c4f2ed3e9498e54e65e16e656abdd4f87a5
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-10-11 13:36:20 +0900

    Update __init__.py

--- trypython/py38/__init__.py
+++ trypython/py38/__init__.py
@@ -9,7 +9,8 @@ def regist_modules(m: Dict[str, Callable[[], None]]):
     ########################################
     # trypython.py38
     ########################################
-    if sys.version.startswith('3.8'):
+    ver = sys.version_info
+    if ver.major == 3 and ver.minor >= 8:
         from trypython.py38 import assignment_expression, fstring_debug
 
         m["py38_assignment_expression"] = assignment_expression.go
